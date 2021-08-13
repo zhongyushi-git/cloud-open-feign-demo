e181d5f13f72c04c2915151e5708a99106d8ca20
zhongyushi <1916008067@qq.com>
2021-08-13 21:39:46 +0800

    文件上传说明

--- cloud-consumer80/src/main/java/com/zys/cloud/service/UserServiceClient.java
+++ cloud-consumer80/src/main/java/com/zys/cloud/service/UserServiceClient.java
@@ -2,10 +2,9 @@ package com.zys.cloud.service;
 
 import com.zys.cloud.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 //指定微服务名称
 @FeignClient(value = "cloud-consul-provider")
@@ -22,4 +21,8 @@ public interface UserServiceClient {
 
     @PostMapping("/user/add")
     String addUser(@RequestBody User user);
+
+    //文件上传
+    @PostMapping(value = "/user/fileUpload",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    String fileUpload(@RequestPart("file")MultipartFile file);
 }
